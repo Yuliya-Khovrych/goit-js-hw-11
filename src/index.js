c90ce32abb5b6eda7c2bbc6c7253595cbbb17e5b
Yuliya-Khovrych <ulia.voitishenko@gmail.com>
2022-10-25 16:34:32 +0300

    task1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import './css/styles.css';
 import { fetchImages } from './js/axios';
-import { markupGallery } from './js/markupGallery';
+import { markupGallery } from './js/markup-gallery';
 
 const searchForm = document.querySelector('.search-form');
 console.log(searchForm);
@@ -13,14 +13,13 @@ console.log(input);
 const gallery = document.querySelector('.gallery');
 console.log(gallery);
 
-let query = '';
 let simpleLightBox;
 
 searchForm.addEventListener('submit', onSearchForm);
 
 function onSearchForm(evt) {
   evt.preventDefault();
-  query = evt.currentTarget.searchQuery.value.trim();
+  const query = evt.currentTarget.searchQuery.value.trim();
   console.log(query);
 
   gallery.innerHTML = '';
@@ -33,7 +32,6 @@ function onSearchForm(evt) {
 
   fetchImages(query)
     .then(({ data }) => {
-      console.log(data);
       if (data.totalHits === 0) {
         Notiflix.Notify.info(
           'Sorry, there are no images matching your search query. Please try again.'
--- src/js/axios.js
+++ src/js/axios.js
@@ -1,18 +1,21 @@
 import axios from 'axios';
 
+const axios = require('axios').default;
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '30758964-b2abbca2686476de38f5cecb6';
 
-const params = new URLSearchParams({
-  key: KEY,
-  q: 'query',
-  imageType: 'photo',
-  orientation: 'horizontal',
-  safesearch: 'true',
-});
+// const params = new URLSearchParams({
+//   key: KEY,
+//   q: 'searchQuery',
+//   imageType: 'photo',
+//   orientation: 'horizontal',
+//   safesearch: 'true',
+// });
 
-export async function fetchImages(params) {
-  const response = await axios.get('${BASE_URL}${params}');
+export async function fetchImages(searchQuery, page, perPage) {
+  const response = await axios.get(
+    '${BASE_URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}'
+  );
   console.log(response);
   return response;
 }
